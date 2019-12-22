f4594c3a3568a761fa987bef4f11f527c7bd989f
Jarrett <jarrettphale@gmail.com>
2019-12-22 00:58:35 -0500

    catching errors and formatting for readability

--- Server.js
+++ Server.js
@@ -17,54 +17,76 @@ client.on("error", async (error) => {
 
 client.on("message", (message) => {
 	if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
+	if (message.channel.type === "dm") return;
 
-  let prefix = "-tarkov";
-  let messageArray = message.content.split(" ");
-  let command = messageArray[0];
-  let args = messageArray.slice(1);
+	let prefix = "-tarkov";
+	let messageArray = message.content.split(" ");
+	let command = messageArray[0];
+	let args = messageArray.slice(1);
 
-  if (!command.startsWith(prefix)) return;
+	if (!command.startsWith(prefix)) return;
 
-  if(args[0] === 'map'){
-    switch(args[1]){
-      case 'customs':
-        message.channel.send("Customs Map", { files: ["./static/Customs.jpg"] })
-        break;
-      
-      case 'factory':
-        message.channel.send("Factory Map", { files: ["./static/Factory.png"] })
-        break;
-        
-      case 'interchange':
-        message.channel.send("Interchange Map", { files: ["./static/Interchange.jpg"] })
-        break;
-      
-      case 'labs':
-        message.channel.send("Labs Map", { files: ["./static/Labs.jpeg"] })
-        break;
-      
-      case 'reserve':
-        message.channel.send("Reserve Map", { files: ["./static/Reserve.jpg"] })
-        break;
-      
-      case 'shoreline':
-        message.channel.send("Shoreline Map", { files: ["./static/Shoreline.jpg"] })
-        break;
-      
-      case 'woods':
-        message.channel.send("Woods Map", { files: ["./static/Woods.jpg"] })
-        break;
+	if (args[0] === "map") {
+		switch (args[1]) {
+			case "customs":
+				message.channel
+					.send("Customs Map", { files: ["./static/Customs.jpg"] })
+					.catch((error) => console.log(error));
+				break;
 
-      default:
-        message.channel.send("Sorry, map not found. Try -tarkov help")
-        break;
-      }
-  }
+			case "factory":
+				message.channel
+					.send("Factory Map", { files: ["./static/Factory.png"] })
+					.catch((error) => console.log(error));
+				break;
 
-  if(args[0] === 'hideoutreqs'){
-    message.channel.send("Hideout Requirements", { files: ["./static/Hideout-reqs.jpg"] })
-  }
+			case "interchange":
+				message.channel
+					.send("Interchange Map", { files: ["./static/Interchange.jpg"] })
+					.catch((error) => console.log(error));
+				break;
+
+			case "labs":
+				message.channel
+					.send("Labs Map", { files: ["./static/Labs.jpeg"] })
+					.catch((error) => console.log(error));
+				break;
+
+			case "reserve":
+				message.channel
+					.send("Reserve Map", { files: ["./static/Reserve.png"] })
+					.catch((error) => console.log(error));
+				break;
+
+			case "shoreline":
+				message.channel
+					.send("Shoreline Map", { files: ["./static/Shoreline.jpg"] })
+					.catch((error) => console.log(error));
+				break;
+
+			case "woods":
+				message.channel
+					.send("Woods Map", { files: ["./static/Woods.jpg"] })
+					.catch((error) => console.log(error));
+				break;
+
+			default:
+				message.channel
+					.send("Sorry, map not found. Try -tarkov help")
+					.catch((error) => console.log(error));
+				break;
+		}
+	}
+
+	if (args[0] === "hideoutreqs") {
+		message.channel.send("Hideout Requirements", {
+			files: ["./static/Hideout-reqs.jpg"]
+		});
+	}
+
+	if(args[0] === 'gavinisgay'){
+		message.channel.send("/tts gavin is a gay stupid retarded dyslexic polio ridden sexually aggressive indie go go funded niggerman")
+	}
 });
 
 client.login(process.env.TOKEN);
